e61016c6812f7ddf3b02deb6f9973a5a1db18b37
Rohan Desai <rohand.rl@gmail.com>
2016-09-07 00:30:20 -0700

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,6 @@ module.exports = {
         test: /\.js?$/,
         loaders: ['babel'],
         include: path.join(__dirname, 'src'),
-        exclude: path.join(__dirname, 'node_modules'),
       },
     ],
   },
